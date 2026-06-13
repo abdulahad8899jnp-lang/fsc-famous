12dd7d53b64cec73035572a4e3946bf649307d11
Abdul Ahad <your-email@gmail.com>
2026-06-13 09:06:14 +0530

    Fix import syntax error

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -100,7 +100,10 @@ const removeCustomSize = (index) => {
       }))
     );
   };
-  
+  const fetchUserCount = async () => {
+  const snap = await getDocs(collection(db, "users"));
+  setUserCount(snap.size);
+};
 
   const uploadImage = async (file, index) => {
 
@@ -436,7 +439,9 @@ const handleLogout = async () => {
 >
   All Users
 
-  
+  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-2 py-1 rounded-full">
+    {userCount}
+  </span>
 </button>
 <button
   onClick={() => navigate("/admin/login")}
